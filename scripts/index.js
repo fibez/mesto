fa4133c5a0b9a337f676f8c7487add7c0271e8c3
Artem Eliseev <aeliseevd@yandex.ru>
2022-11-21 15:49:47 +0300

    bugfix: layout and styles refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
-"use strict";
+'use strict';
 //
 //
 //
-const editButtonElement = document.querySelector(".profile__edit-button");
-const popupElement = document.querySelector(".popup");
-const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
-const popupNameInputElement = popupElement.querySelector(".popup__input_type_name");
-const popupProfessionInputElement = popupElement.querySelector(".popup__input_type_profession");
-const popupSubmitButton = popupElement.querySelector(".popup__save-button");
-const profileName = document.querySelector(".profile__name");
-const profileProfession = document.querySelector(".profile__description");
+const editButtonElement = document.querySelector('.profile__edit-button');
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupNameInputElement = popupElement.querySelector('.popup__input_type_name');
+const popupProfessionInputElement = popupElement.querySelector('.popup__input_type_profession');
+const popupSubmitButton = popupElement.querySelector('.popup__save-button');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__description');
 //
 //
 //
 const openPopup = function () {
-  popupElement.style.visibility = "visible";
+  popupElement.classList.add('popup__opened');
   popupNameInputElement.value = profileName.textContent;
   popupProfessionInputElement.value = profileProfession.textContent;
 };
 
 const closePopup = function (event) {
   if (event.target === popupCloseButtonElement || event.target === event.currentTarget) {
-    popupElement.style.visibility = "hidden";
+    popupElement.classList.remove('popup__opened');
   }
 };
 
 const submitProfileChanges = function (event) {
-  if (popupNameInputElement.value !== "" && popupProfessionInputElement.value !== "") {
+  if (popupNameInputElement.value !== '' && popupProfessionInputElement.value !== '') {
     profileName.textContent = popupNameInputElement.value;
     profileProfession.textContent = popupProfessionInputElement.value;
     closePopup(event);
@@ -36,7 +36,7 @@ const submitProfileChanges = function (event) {
 //
 //
 //
-editButtonElement.addEventListener("click", openPopup);
-popupCloseButtonElement.addEventListener("click", closePopup);
-popupElement.addEventListener("click", closePopup);
-popupSubmitButton.addEventListener("click", submitProfileChanges);
+editButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopup);
+popupSubmitButton.addEventListener('click', submitProfileChanges);
