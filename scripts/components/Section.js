33913e5289349c3875827a3c8ef25f45ff182857
Artem Eliseev <aeliseevd@yandex.ru>
2023-02-05 18:15:44 +0300

    feat/add new Section class, remove old card render

---
+++ scripts/components/Section.js
@@ -0,0 +1,19 @@
+class Section {
+  constructor({ renderer }, selector) {
+    // this._data = data;
+    this._renderer = renderer;
+    this._container = document.querySelector(selector);
+  }
+
+  addItem(element) {
+    this._container.prepend(element);
+  }
+
+  renderElement(data) {
+    data.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+}
+
+export { Section };
